9dca41c9505bb6975ee04acbc883c68664a353d4
Astral-Script <antonyderon@gmail.com>
2025-11-01 15:00:14 +0800

    Update Astral-ScriptV2.lua

--- Astral-ScriptV2.lua
+++ Astral-ScriptV2.lua
@@ -234,19 +234,6 @@ for _, name in ordered_fun_scripts do
             starlight:Destroy()
         end,
      }, name)
-
-name = 'Krnl',
-check = function()
-  return globalExists('Krnl')
-      or globalExists('Krnl')
-      or hasRequestVariant('Krnl')
-      or (
-          type(identifyexecutor) == 'function'
-          and pcall(function()
-              return identifyexecutor(): lower():find('Krnl')
-           end)
-      )
-end,
-    
 end
+
 
